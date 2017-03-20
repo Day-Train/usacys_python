dc06fe74e15ce772a0bccdac4ac90dc8e73e7481
Julianna Rodriguez <julianna.m.rodriguez3.mil@mail.mil>
2017-03-20 08:45:37 -0400

    moved file from previous location

---
+++ exercises/ex1_3_2/objects/hosts.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+
+class Host(object):
+    def __init__(self, ip):
+        self.ip = ip
+        self.openports = set()
+
+    def add_port(self, port):
+        self.openports.add(port)
