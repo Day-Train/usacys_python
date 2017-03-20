e7d642486eef8ae4ad4e17679d05aa38192d0b23
Julianna Rodriguez <julianna.m.rodriguez3.mil@mail.mil>
2017-03-20 08:23:53 -0400

    moved file from previous location

---
+++ exercises/ex1_1_3.py
@@ -0,0 +1,13 @@
+import ad
+import uncertainties
+
+from pyprimes import nth_prime
+from hashlib import sha1
+
+a = nth_prime(70)
+
+u = uncertainties.ufloat(a,5)
+
+s = ad.math.sin(u.nominal_value)
+
+print(sha1(str(s)).hexdigest())
