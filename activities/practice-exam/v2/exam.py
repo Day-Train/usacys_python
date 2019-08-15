396c1993e36188bd82a877d5eb9e5930db01569e
nicholas.r.yost <usacys@local>
2019-08-15 12:38:46 -0400

    "automated publish"

--- activities/practice-exam/v2/exam.py
+++ activities/practice-exam/v2/exam.py
@@ -38,7 +38,8 @@ def q5(integer, limit):
     '''
     Given an integer and limit, return a list of even multiples of the
     integer up to and including the limit. For example, if integer==3 and
-    limit==30, the returned list should be [6,12,18,24,30].
+    limit==30, the returned list should be [0,6,12,18,24,30]. Note, 0 is
+    a multiple of any integer except 0 itself.
     '''
     pass
 
