186cda84ec314ffe019a6c9b0faf8a9e056feae4
usacys <usacys@local>
2019-09-04 13:28:18 -0400

    Changes to be committed: 	modified:   check.py 	modified:   deliverable.py 	copied:     deliverable.py -> deliverablealt.py 	modified:   ../part4/deliverable.py 	new file:   ../part4/test.txt

---
+++ activities/pe1/part3/deliverablealt.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+def steg_encode(msg, cover):
+    '''LSB encodes a message
+    Args:
+        msg (str): a string message to encode
+        cover (list): list of strings representing integers in the range [0-255]
+    Returns:
+        None
+    '''
+
+    #Convert msg to char list
+    msglist = list(msg)
+
+    coverlistbin = []
+    msglistbin = []
+    
+    #Convert integer values to binary
+    for item in cover:
+        coverlistbin.append(format(int(item), '0>8b'))
+
+    #Convert char list to binary
+    for char in msglist:
+        msglistbin.append(format(ord(char),'0>8b'))
+    #print(msglistbin)
+
+    #For char at index i, position j in msglistbin
+    #Take string at index i in coverlistbin and replace element [-1] with char
+    #char       cover
+    # i,j       i,-1
+    # i,j+1     i+1,-1
+    #...
+    # i,j+7     i+7,-1
+    # i+1,j     i+8,-1
+    #...
+    # i+1,j+7   i+15,-1
+    #...
+    #...
+    # i+n,j+7   i+8*n-1,-1
+
+    #This list will store the modified cover in binary
+    output = []
+
+    i = 0
+    for i in range(len(msglistbin)):
+        k = 0
+        while k < 8:
+            word = coverlistbin[i][0:7] + msglistbin[i][k]
+            output.append(word)
+            k += 1
+        i += 1
+
+    #This list will store the modified cover as integers
+    final = []
+
+    for byte in output:
+        final.append(int(byte,2))
+
+    print(msglistbin)
+    print(coverlistbin)
+    print(output)
+    print(final)
+    pass
+
+def steg_decode(stego):
+    '''LSB decodes a message
+    Args:
+        stego (list): list of strings representing integers in the range [0-255]
+    Returns:
+        str: message that was decoded
+    '''
+    stegolistbin = []
+
+    for value in stego:
+        stegolistbin.append(format(int(value),'0>8b'))
+
+    msgbin = ''
+
+    i = 0
+    for i in range(len(stego)):
+        msgbin += stegolistbin[i][-1]
+        i += 1
+
+    msglistbin = [msgbin[k:k+8] for k in range(0, len(stego), 8)]
+
+    charnum = []
+    for item in msglistbin:
+        charnum.append(chr(int(item,2)))
+
+    output = ''.join(charnum)
+
+    print(output)
+    return output
+    pass
+
+if __name__ == '__main__':
+	pass
+
+#steg_encode('he',['250']*16)
+#steg_decode([250, 251, 251, 250, 251, 250, 250, 250, 250, 251, 251, 250, 250, 251, 250, 251])
--- activities/pe1/part4/deliverable.py
+++ activities/pe1/part4/deliverable.py
@@ -9,6 +9,28 @@ def read_pgm(filename):
             1st element is a list of PGM header values as strings
             2nd element is a list of pixel intensities as strings
     '''
+
+    headervalues = []
+    pixelvalues = []
+
+    with open(filename) as fp:
+        data = fp.read()
+        raw = data.split()
+
+        i = 0
+        while i < 4:
+            headervalues.append(raw[i])
+            i += 1
+
+        for j in range(i,len(raw)):
+            pixelvalues.append(raw[j])
+            j += 1
+    
+    readtuple = (headervalues, pixelvalues)
+    return readtuple
+
+    #print(readtuple)
+
     pass
 
 def write_pgm(filename,content):
@@ -21,6 +43,17 @@ def write_pgm(filename,content):
     Returns:
         None
     '''
+    with open(filename, 'w') as fp:
+        i = 0
+        while i < 4:
+            fp.write(content[0][i] + '\n')
+            i += 1
+
+        j = 0
+        while j < len(content[1]):
+            fp.write(content[1][j] + '\n')
+            j += 1
+
     pass
 
 def invert(content):
@@ -32,7 +65,22 @@ def invert(content):
     Returns:
         None
     '''
+
+    inverted = []
+
+    for pixel in content[1]:
+        inverted.append(str(255 - int(pixel)))
+
+    invertedtuple = (content[0],inverted)
+
+    #return invertedtuple
     pass
 
 if __name__ == '__main__':
-	pass
+    contents = read_pgm('plain.pgm')
+    invert(contents)
+    write_pgm('inverted.pgm',contents)
+    
+    pass
+
+
