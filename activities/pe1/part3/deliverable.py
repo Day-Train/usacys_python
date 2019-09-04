07bbf9b4876d09105553639781e209b0724eb746
usacys <usacys@local>
2019-09-04 10:00:34 -0400

    Changes to be committed: 	modified:   deliverable.py 	new file:   ../../../workspace/Tutorials/whatever.py

--- activities/pe1/part3/deliverable.py
+++ activities/pe1/part3/deliverable.py
@@ -15,16 +15,51 @@ def steg_encode(msg, cover):
     coverlistbin = []
     msglistbin = []
     
-    #Convert cover values to bit
+    #Convert integer values to binary
     for item in cover:
         coverlistbin.append(format(int(item), '0>8b'))
-    print(coverlistbin)
 
-    #Convert char list to bin list
+    #Convert char list to binary
     for char in msglist:
         msglistbin.append(format(ord(char),'0>8b'))
-    print(msglistbin)
+    #print(msglistbin)
 
+    #For char at index i, position j in msglistbin
+    #Take string at index i in coverlistbin and replace element [-1] with char
+    #char       cover
+    # i,j       i,-1
+    # i,j+1     i+1,-1
+    #...
+    # i,j+7     i+7,-1
+    # i+1,j     i+8,-1
+    #...
+    # i+1,j+7   i+15,-1
+    #...
+    #...
+    # i+n,j+7   i+8*n-1,-1
+
+    #This list will store the modified cover in binary
+    output = []
+
+    i = 0
+    for i in range(len(msglistbin)):
+        k = 0
+        while k < 8:
+            word = coverlistbin[i][0:7] + msglistbin[i][k]
+            output.append(word)
+            k += 1
+        i += 1
+
+    #This list will store the modified cover as integers
+    final = []
+
+    for byte in output:
+        final.append(int(byte,2))
+
+    print(msglistbin)
+    print(coverlistbin)
+    print(output)
+    print(final)
     pass
 
 def steg_decode(stego):
@@ -34,9 +69,32 @@ def steg_decode(stego):
     Returns:
         str: message that was decoded
     '''
+    stegolistbin = []
+
+    for value in stego:
+        stegolistbin.append(format(int(value),'0>8b'))
+
+    msgbin = ''
+
+    i = 0
+    for i in range(len(stego)):
+        msgbin += stegolistbin[i][-1]
+        i += 1
+
+    msglistbin = [msgbin[k:k+8] for k in range(0, len(stego), 8)]
+
+    charnum = []
+    for item in msglistbin:
+        charnum.append(chr(int(item,2)))
+
+    output = ''.join(charnum)
+
+    print(output)
+    return output
     pass
 
 if __name__ == '__main__':
 	pass
 
-steg_encode('hello',['250']*16)
+#steg_encode('he',['250']*16)
+steg_decode([250, 251, 251, 250, 251, 250, 250, 250, 250, 251, 251, 250, 250, 251, 250, 251])
